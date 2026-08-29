33497aad1338ee4f2ab63c716e7ca41a0aa9b5f7
oczygan <322202321+oczygan@users.noreply.github.com>
2026-08-29 11:38:56 +0200

    setup-file für OliClick

---
+++ setup.py
@@ -0,0 +1,13 @@
+# setup.py
+from distutils.core import setup
+import py2exe
+
+#setup(console=["test2.py"])
+setup(zipfile=None,
+      windows=["OliClick.py"])
+
+#
+# Uebersetzung mit:
+# c:\programme\python_26\python setup.py py2exe
+# C:\Python27\python setup.py py2exe
+#
